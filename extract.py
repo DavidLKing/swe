346ce3f040ffd5e82db7c34c3af3c81e7a644c02
David L. King <king.david.lee@gmail.com>
2017-10-24 16:28:08 -0400

    completed extraction, next is formatting

--- extract.py
+++ extract.py
@@ -1,8 +1,8 @@
-from __future__ import print_function
+# from __future__ import print_function
 import sys
 import argparse
 import pdb
-import cPickle as pkl
+# import cPickle as pkl
 import bz2 
 
 class extract:
@@ -17,25 +17,42 @@ class extract:
 
     def load_file(self):
         xmlfile = self.args.input
-        xf = bz2.BZ2File(xmlfile, 'r')
+        # xf = bz2.BZ2File(xmlfile, 'r')
+        xf = bz2.open(xmlfile, 'rt')
         line_count = 0
         nouns = []
         verbs = []
         adjec = []
         wtype = ''
+        group = []
         for line in xf:
             line_count += 1
-            group = []
-            if line.startswith('}}') and group != [] and type != '':
-                if wtype = 'n':
-                    nouns.append(group)
-                elif wtype = 'a':
-                    adjec.append(group)
-                else: 
-                    verbs.append(group)
-                group = []
-                wtype = ''
-            elif line.startswith('{{Deutsch') and 
+            line = line.split()
+            if len(line) > 0:
+                if line[0] == '}}' and group != [] and type != '':
+                    if wtype == 'n':
+                        nouns.append(group)
+                    elif wtype == 'a':
+                        adjec.append(group)
+                    else:
+                        verbs.append(group)
+                    group = []
+                    wtype = ''
+                elif line[0].startswith('|') and wtype != '':
+                    group.append(' '.join(line))
+                elif len(line) > 2:
+                    if line[2] == 'Übersicht' and line[0] == '{{Deutsch':
+                        group.append(' '.join(line))
+                        if line[1] == 'Substantiv':
+                            wtype = 'n'
+                        elif line[1] == 'Verb':
+                            wtype = 'v'
+                        else:
+                            wtype = 'a'
+        print("Total lines", line_count)
+        print("Total nouns", len(nouns))
+        print("Total verbs", len(verbs))
+        print("Total adjectives", len(adjec))
 
 
 if __name__ == '__main__':
