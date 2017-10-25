d6895e77c852738b3285ad8636eeba6fc8ff6819
David L. King <king.david.lee@gmail.com>
2017-10-25 19:05:43 -0400

    basic formatting down. Needs mapping to sigmorphon features

--- extract.py
+++ extract.py
@@ -1,8 +1,8 @@
-# from __future__ import print_function
+#!/usr/bin/env python3
+
 import sys
 import argparse
 import pdb
-# import cPickle as pkl
 import bz2 
 
 class extract:
@@ -25,19 +25,28 @@ class extract:
         adjec = []
         wtype = ''
         group = []
+        citeform = ''
         for line in xf:
             line_count += 1
             line = line.split()
             if len(line) > 0:
-                if line[0] == '}}' and group != [] and type != '':
+                # if line[0] == '}}' and group != [] and wtype != '':
+                # TODO this is a hack to work on the test file
+                if line[0] == '}}' and group != [] and wtype != '':
+                    group.append(citeform)
                     if wtype == 'n':
                         nouns.append(group)
                     elif wtype == 'a':
                         adjec.append(group)
+                    # elif wtype == 'v':
                     else:
                         verbs.append(group)
+                    # ignore the rest
                     group = []
                     wtype = ''
+                    citeform = ''
+                elif line[0] == '==' and line[-1] == '==':
+                    citeform = ' '.join(line)
                 elif line[0].startswith('|') and wtype != '':
                     group.append(' '.join(line))
                 elif len(line) > 2:
@@ -47,14 +56,32 @@ class extract:
                             wtype = 'n'
                         elif line[1] == 'Verb':
                             wtype = 'v'
-                        else:
+                        elif line[1] == 'Adjektiv':
                             wtype = 'a'
+                        # ignore Nachname/Vorname Übersicht type tables
+                        else:
+                            wtype = ''
         print("Total lines", line_count)
         print("Total nouns", len(nouns))
         print("Total verbs", len(verbs))
         print("Total adjectives", len(adjec))
+        return nouns, verbs, adjec
 
+    def rewrite_verb(self, paradigms):
+        for p in paradigms:
+            inf = p[-1].split()[1]
+            assert(p[-1].split()[2] == '({{Sprache|Deutsch}})')
+            for cell in p[1:-1]:
+                cell = cell.split('=')
+                try:
+                    assert(len(cell) >= 0)
+                except:
+                    print(cell)
+                    pdb.set_trace()
+                line = '\t'.join([inf, '-'.join(cell[0:-1])[1:], cell[-1]])
+                print(line)
 
 if __name__ == '__main__':
     e = extract()
-    e.load_file()
+    n, v, a = e.load_file()
+    e.rewrite_verb(v)
